322e186ce46ff52289308df21afaa72c6ed834aa
MarinaDelbeeva <mariner41165@gmail.com>
2021-06-30 22:39:28 +0300

    corrections in homework 3

--- src/test/java/ru/training/at/hw3/pages/DifferentElementsPage.java
+++ src/test/java/ru/training/at/hw3/pages/DifferentElementsPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
@@ -13,6 +14,13 @@ public class DifferentElementsPage {
     private final WebDriver driver;
     private final Actions action;
 
+    @FindBy (className = "label-checkbox")
+    private List<WebElement> listOfCheckboxes;
+    @FindBy (className = "label-radio")
+    private List<WebElement> listOfRadios;
+    @FindBy (className = "colors")
+    private WebElement colors;
+
     public DifferentElementsPage(WebDriver webDriver) {
         driver = webDriver;
         PageFactory.initElements(webDriver, this);
@@ -20,18 +28,15 @@ public class DifferentElementsPage {
     }
 
     public void activateCheckboxes() {
-        List<WebElement> listOfCheckboxes = driver.findElements(By.className("label-checkbox"));
         action.moveToElement(listOfCheckboxes.get(0)).click().perform();
         action.moveToElement(listOfCheckboxes.get(2)).click().perform();
     }
 
     public void selectSelenRadiobutton() {
-        List<WebElement> listOfRadios = driver.findElements(By.className("label-radio"));
         action.moveToElement(listOfRadios.get(3)).click().perform();
     }
 
     public void selectYellowColor() {
-        WebElement colors = driver.findElement(By.className("colors"));
         WebElement selectElem = colors.findElement(By.className("uui-form-element"));
         new Select(selectElem).selectByIndex(3);
     }
--- src/test/java/ru/training/at/hw3/pages/HomePage.java
+++ src/test/java/ru/training/at/hw3/pages/HomePage.java
@@ -22,6 +22,22 @@ public class HomePage {
     private WebElement passwordField;
     @FindBy (id = "login-button")
     private WebElement loginButton;
+    @FindBy (id = "user-name")
+    private WebElement username;
+    @FindBy (xpath = "//ul[@class='uui-navigation nav navbar-nav m-l8']")
+    private List<WebElement> itemsOnHeader;
+    @FindBy (xpath = "//ul[@class='sidebar-menu left']")
+    private List<WebElement> sideBarItems;
+    @FindBy (className = "benefit-icon")
+    private List<WebElement> images;
+    @FindBy (className = "benefit-txt")
+    private List<WebElement> textsUnderImages;
+    @FindBy (tagName = "iframe")
+    private List<WebElement> iframes;
+    @FindBy (className = "dropdown-toggle")
+    private WebElement serviceButton;
+    @FindBy (xpath = "//a[text()='Different elements']")
+    private WebElement differentElements;
 
     public HomePage(WebDriver webDriver) {
         driver = webDriver;
@@ -37,33 +53,26 @@ public class HomePage {
     }
 
     public String getUsername() {
-        WebElement username = driver.findElement(By.id("user-name"));
         return username.getText();
     }
 
     public List<String> itemsOnHeaderToStrings() {
-        List<WebElement> itemsOnHeader =
-            driver.findElements(By.xpath("//ul[@class='uui-navigation nav navbar-nav m-l8']"));
         return itemsOnHeader.stream().map(WebElement::getText).collect(Collectors.toList());
     }
 
     public List<String> sideBarItems() {
-        List<WebElement> sideBarItems = driver.findElements(By.xpath("//ul[@class='sidebar-menu left']"));
         return sideBarItems.stream().map(WebElement::getText).collect(Collectors.toList());
     }
 
     public int countOfImages() {
-        List<WebElement> images = driver.findElements(By.className("benefit-icon"));
         return images.size();
     }
 
     public int countOfTextsUnderImages() {
-        List<WebElement> textsUnderImages = driver.findElements(By.className("benefit-txt"));
         return (int) textsUnderImages.stream().map(WebElement::getText).count();
     }
 
     public List<WebElement> existingOfIframe() {
-        List<WebElement> iframes = driver.findElements(By.tagName("iframe"));
         return iframes.get(0).findElements(By.id("frame-button"));
     }
 
@@ -72,9 +81,7 @@ public class HomePage {
     }
 
     public void openDifferentElementsPage() {
-        WebElement serviceButton = driver.findElement(By.className("dropdown-toggle"));
         new Actions(driver).moveToElement(serviceButton).click().perform();
-        WebElement differentElements = driver.findElement(By.xpath("//a[text()='Different elements']"));
         new Actions(driver).moveToElement(differentElements).click().perform();
     }
 }
